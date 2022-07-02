39b5e4d5ae30358e3d1f24425d9f14b377e9c15a
MMallea <40362079+MMallea@users.noreply.github.com>
2022-07-02 18:08:32 -0400

    Capture Points

---
+++ Assets/CapturePoint.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapturePoint : MonoBehaviour
+{
+    public ParticleSystem captureCircleParticles;
+
+    private void Start()
+    {
+    }
+}
